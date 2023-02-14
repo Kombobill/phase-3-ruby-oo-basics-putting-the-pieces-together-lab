c924370250cd247c547d342b20f10d026080e601
Kombobill <billmadru25@gmail.com>
2023-02-14 10:45:23 +0300

    done

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,19 @@
-class Book
-    attr_accessor :author, :page_count, :genre, :title
-  
-    def initialize(title)
-      @title = title
-      @author = author
-      @page_count = page_count
-      @genre = genre
-    end
-def turn_page
-    puts "Flipping the page...wow, you read fast!"
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+    attr_reader :brand
+
+
+def initialize(brand)
+    @brand = brand
+    @color = color
+    @size = size
+    @material = material
+    @condition = condition
 end
+ def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+
     
+ end
 end
