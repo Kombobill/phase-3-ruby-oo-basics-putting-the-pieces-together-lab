7e4e882b6b2bb0bca0a8ca44644343052a8c72a8
Kombobill <billmadru25@gmail.com>
2023-02-14 10:05:58 +0300

    completed assignment

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
-# Make your shoe class here!
+class Book
+    attr_accessor :author, :page_count, :genre, :title
+  
+    def initialize(title)
+      @title = title
+      @author = author
+      @page_count = page_count
+      @genre = genre
+    end
+def turn_page
+    puts "Flipping the page...wow, you read fast!"
+end
+    
+end
